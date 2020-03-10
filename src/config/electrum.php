b229e21ffb71a9ad7d74cad6b7150f3e7cc20df8
Takuya Nakaike <nakaike@jp.ibm.com>
2020-03-10 11:30:29 +0900

    Specify electrum using environment variables

--- src/config/electrum.php
+++ src/config/electrum.php
@@ -1,11 +1,17 @@
 <?php
 
+$host = getenv('ELECTRUM_DAEMON_HOST', true) ?: '127.0.0.1';
+$user = getenv('ELECTRUM_DAEMON_USER', true) ?: 'electrum';
+$password = getenv('ELECTRUM_DAEMON_PASSWORD', true) ?: '';
+
+$electrumhost = 'http://'.$user.':'.$password.'@'.$host;
+
 return [
-    'host'          => 'http://127.0.0.1',
+    'host'          => $electrumhost,
     'port'          => '7777',
     'web_interface' => [
-        'enabled'    => false,
-        'currency'   => 'EUR',
+        'enabled'    => true,
+        'currency'   => 'USD',
         'middleware' => ['web', 'auth'],
         'prefix'     => 'electrum',
     ],
